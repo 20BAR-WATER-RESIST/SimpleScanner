2eeeb78681e4d7a32fbbc576e6c3c6d70ebb843b
Jakub Pilarski <pilarski67@gmail.com>
2024-04-22 08:51:07 +0200

    Small changes: analyzer added

--- SimpleScanner/ScannerPage.xaml.cs
+++ SimpleScanner/ScannerPage.xaml.cs
@@ -4,6 +4,7 @@ namespace SimpleScanner;
 
 public partial class ScannerPage : ContentPage
 {
+    private int TextInsertedCounter { get; set; } = 0;
     public ScannerPage()
     {
         InitializeComponent();
@@ -18,8 +19,48 @@ public partial class ScannerPage : ContentPage
         CbEntry.Focus();
     }
 
-    private void CbEntry_TextChanged(object? sender, TextChangedEventArgs e)
+    private async void CbEntry_TextChanged(object? sender, TextChangedEventArgs e)
     {
+        TextInsertedCounter++;
         
+        if (TextInsertedCounter == 1)
+        {
+            //await AnalyzingStage();
+            await WaitMethod(3);
+        }
+    }
+    
+    private async Task WaitMethod(int seconds)
+    {
+        CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+        CancellationToken cancellationToken = cancellationTokenSource.Token;
+
+        await AnalyzingStage(cancellationToken);
+        
+        await Task.Delay(seconds * 1000);
+        cancellationTokenSource.Cancel();
+        
+        return;
+    }
+
+    private async Task AnalyzingStage(CancellationToken token)
+    {
+        StageInfoLabel.Text = "Analyzing";
+
+        for(int i = 0; i < 4;)
+        {
+            await Task.Delay(600);
+            if (i < 3)
+            {
+                StageInfoLabel.Text += ".";
+                i++;
+            }
+            else
+            {
+                StageInfoLabel.Text = "Analyzing";
+                i = 0;
+            }
+        }
+        return;
     }
 }
